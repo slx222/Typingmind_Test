5ba5182c2613566384414b275ff461b943f33cce
slx222 <118348786+slx222@users.noreply.github.com>
2024-11-07 12:54:30 +0100

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Test Extension
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Just a test
+// @author       You
+// @match        https://www.typingmind.com/*
+// @grant        none
+// ==/UserScript==
+
+console.log("Test extension loaded!");
+
+alert("Hello from the test extension!");
